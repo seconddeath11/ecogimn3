2a15d8082b6c06ab7625794d0a4abec705ec62a2
Diana <stels270799@gmail.com>
2021-09-20 13:10:26 +0300

    added bunch of stuff

--- forms.py
+++ forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, IntegerField
 from wtforms import MultipleFileField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileRequired, FileField
 
 
 class Form(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    school = SelectField(choices=[('Гимназия 3', 'Гимназия 3'), ('СШ №11', 'СШ №11')])
-    class_no = StringField('class_no', validators=[DataRequired()])
-    class_letter = SelectField(choices=[('А', 'A'), ('Б', 'Б')])
-    remember_me = BooleanField('Remember Me')
-    photo = MultipleFileField()
-    submit = SubmitField('Sign In')
+    username = StringField('ФИО', validators=[DataRequired()])
+    school = SelectField('Школа', choices=[('Гимназия 3', 'Гимназия 3'), ('СШ №11', 'СШ №11')])
+    class_no = IntegerField('Номер класса', validators=[DataRequired()])
+    class_letter = SelectField('Буква класса', choices=[('А', 'A'), ('Б', 'Б')])
+    remember = BooleanField('Согласие на ')
+    photo = MultipleFileField('Прикрепить фото')
+    submit = SubmitField('Отправить')
