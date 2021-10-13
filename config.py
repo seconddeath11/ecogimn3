edb9c5cfea3830d3cdc87e4a94de94234a5782da
Diana <stels270799@gmail.com>
2021-10-13 17:29:13 +0300

    First python app

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USERNAME = os.environ['USERNAME']
 MAIL_PASSWORD = os.environ['PASSWORD']
-SECRET_STRING = os.environ['SECRET_STRING']
+SECRET_KEY = os.environ['SECRET_KEY']
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
