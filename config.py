5fd4d3dc746bde0258587f6529efe516cbe2fe56
Diana <stels270799@gmail.com>
2021-10-18 15:25:54 +0300

    fixed secret key

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USERNAME = os.getenv('USERNAME')
 MAIL_PASSWORD = os.getenv('PASSWORD')
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
